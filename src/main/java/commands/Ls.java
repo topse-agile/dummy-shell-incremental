4d8b4ed6a3f09a0e756f119ed6508a6968ba7950
physicist00 <physicist00@gmail.com>
2018-11-21 21:31:24 +0900

    testDisplayFileAndDirectoryWhenSpecifyChildDirectory2のテストを成功させるための実装中

--- src/main/java/commands/Ls.java
+++ src/main/java/commands/Ls.java
@@ -5,17 +5,88 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ls extends Command {
+	private final String SEPARATOR = "    ";
 
 	@Override
 	public void run(String params) {
 		File homeDir = new File("./test");
-		File[] listFiles = homeDir.listFiles();
+
 		List<String> fileNames = new ArrayList<String>();
-		for (File file : listFiles) {
-			fileNames.add(file.getName());
+
+		String[] split = params.split(" ", 2);
+		String[] options = extractOptions(split);
+		String[] paths = extractPaths(split);
+
+		if (showHiddenFile(options)) {
+			fileNames.add(".");
+			fileNames.add("..");
 		}
-		String str = String.join("    ", fileNames);
+		for (String path : paths) {
+			File file = new File(path);
+
+			File[] listFiles = homeDir.listFiles();
+
+			for (File file : listFiles) {
+				if (showHiddenFile(options)) {
+					fileNames.add(file.getName());
+				} else {
+					if (!isHidden(file)) {
+						fileNames.add(file.getName());
+					}
+				}
+			}
+
+		}
+//		if (showDirectory(split)) {
+//			System.out.println("directory1" + SEPARATOR + "file1");
+//			return;
+//		}
+
+		for (File file : listFiles) {
+			if (showHiddenFile(options)) {
+				fileNames.add(file.getName());
+			} else {
+				if (!isHidden(file)) {
+					fileNames.add(file.getName());
+				}
+			}
+		}
+		String str = String.join(SEPARATOR, fileNames);
+
 		System.out.println(str);
+	}
+
+	private String[] extractPaths(String[] split) {
+		ArrayList<String> arrayList = new ArrayList<String>();
+		for (String param : split) {
+			if (!param.startsWith("-")) {
+				arrayList.add(param);
+			}
+		}
+		return arrayList.toArray(new String[0]);
+	}
+
+	private String[] extractOptions(String[] split) {
+		if (split[0].startsWith("-")) {
+			String[] tmp = new String[1];
+			tmp[0] = split[0];
+			return tmp;
+		} else {
+			return null;
+		}
+	}
+
+	private boolean isHidden(File file) {
+		String fileName = file.getName();
+		return fileName.startsWith(".");
+	}
+
+	private boolean showHiddenFile(String[] split) {
+		return split != null && split[0].equals("-a");
+	}
+
+	private boolean showDirectory(String[] split) {
+		return split[0].equals("sample");
 	}
 
 }
--- src/test/java/commands/LsTest.java
+++ src/test/java/commands/LsTest.java
@@ -14,11 +14,18 @@ import org.junit.Test;
 public class LsTest {
 
 	private Command command;
+	private final String SEPARATOR = "    ";
+
+	private ByteArrayOutputStream out;
 
 	@Before
 	public void setUp() {
 		deleteFolder(new File("./test"));
 		this.command = new Ls();
+
+		out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
 	}
 
 	@Test
@@ -29,12 +36,20 @@ public class LsTest {
 		file1.createNewFile();
 		file2.createNewFile();
 
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
+		this.command.run("");
+
+		assertThat(out.toString(), is("file1.txt" + SEPARATOR + "file2.txt" + System.lineSeparator()));
+	}
+
+	@Test
+	public void testDisplayNoFilesWhenHiddenFileExist() throws IOException {
+		File file1 = new File("./test/.HiddenFile");
+
+		file1.createNewFile();
 
 		this.command.run("");
 
-		assertThat(out.toString(), is("file1.txt    file2.txt" + System.lineSeparator()));
+		assertThat(out.toString(), is("" + System.lineSeparator()));
 	}
 
 	@Test
@@ -47,12 +62,71 @@ public class LsTest {
 		file2.createNewFile();
 		dir.mkdir();
 
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
+		this.command.run("");
+
+		assertThat(out.toString(),
+				is("dir1" + SEPARATOR + "file1.txt" + SEPARATOR + "file2.txt" + System.lineSeparator()));
+	}
+
+	@Test
+	public void testDisplayFilesWhenNoFile() throws IOException {
 
 		this.command.run("");
 
-		assertThat(out.toString(), is("dir1    file1.txt    file2.txt" + System.lineSeparator()));
+		assertThat(out.toString(), is("" + System.lineSeparator()));
+
+	}
+
+	@Test
+	public void testDisplayAllFilesWhenEmptyDirectory() throws IOException {
+
+		this.command.run("-a");
+
+		assertThat(out.toString(), is("." + SEPARATOR + ".." + System.lineSeparator()));
+	}
+
+	@Test
+	public void testDisplayBothOfHiddenFilesAndNormalFilesWhenHiddenFilesAndNormalFilesExist() throws IOException {
+		File file1 = new File("./test/.HiddenFile");
+		File file2 = new File("./test/NormalFile");
+
+		file1.createNewFile();
+		file2.createNewFile();
+
+		this.command.run("-a");
+
+		assertThat(out.toString(), is("." + SEPARATOR + ".." + SEPARATOR + ".HiddenFile" + SEPARATOR + "NormalFile"
+				+ System.lineSeparator()));
+	}
+
+	@Test
+	public void testDisplayFileAndDirectoryWhenSpecifyChildDirectory() throws IOException {
+		File sample = new File("./test/sample");
+		File file1 = new File("./test/sample/file1");
+		File directory1 = new File("./test/sample/directory1");
+
+		sample.mkdir();
+		file1.createNewFile();
+		directory1.mkdir();
+
+		this.command.run("sample");
+
+		assertThat(out.toString(), is("directory1" + SEPARATOR + "file1" + System.lineSeparator()));
+	}
+
+	@Test
+	public void testDisplayFileAndDirectoryWhenSpecifyChildDirectory2() throws IOException {
+		File sample = new File("./test/sample2");
+		File file1 = new File("./test/sample2/file1");
+		File directory1 = new File("./test/sample2/directory1");
+
+		sample.mkdir();
+		file1.createNewFile();
+		directory1.mkdir();
+
+		this.command.run("sample2");
+
+		assertThat(out.toString(), is("directory1" + SEPARATOR + "file1" + System.lineSeparator()));
 	}
 
 	private void deleteFolder(File dir) {
